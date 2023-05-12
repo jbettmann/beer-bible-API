67a1ac14c666811115c93cfc43cb6dc8920a55ae
jbettmann <jordan.bettmann@gmail.com>
2023-05-11 21:28:58 -0600

    added get, put and post methods

--- index.js
+++ index.js
@@ -68,13 +68,15 @@ app.get("/", (req, res) => {
   res.send(`myFlix. All the greats, in one place!`);
 });
 
-let handleError = (err) => {
+let handleError = (res, err) => {
   console.error(err);
   console.log(err);
   res.status(500).send(`Error: ${err}`);
 };
 
 // ************************** BeerBible API ************************************************
+
+//  POST/CREATE REQUEST
 
 /**
  * POST: Creates new user; Username, Password & Email are required fields!
@@ -100,39 +102,104 @@ app.post(
     // field must be formatted as an email address
     check("email", "Email does not appear to be valid").isEmail(),
   ],
-  (req, res) => {
+  async (req, res) => {
     // check the validation object for errors
-    let errors = validationResult(req);
-
+    const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() });
     }
-    let hashedPassword = Users.hashPassword(req.body.password);
     // check if user already exists
-    Users.findOne({ email: req.body.email })
-      .then((user) => {
-        // returns if user exists
-        if (user) {
-          return res
-            .status(400)
-            .send(`An account with ${req.body.email} already exists`);
-          // creates and returns if user DOES NOT exist
-        } else {
-          Users.create({
-            fullName: req.body.fullName,
-            // .create takes and object based on schema
-            username: req.body.username, // remember 'req.body' is request that user sends
-            password: hashedPassword, // Hashes password entered  when registering before storing in MongoDB
-            email: req.body.email,
-            breweries: [],
-          })
-            .then((user) => {
-              res.status(201).json(user);
-            })
-            .catch(handleError);
-        }
-      })
-      .catch(handleError);
+    try {
+      const existingUser = await Users.findOne({ email: req.body.email });
+      if (existingUser) {
+        return res
+          .status(400)
+          .send(`An account with ${req.body.email} already exists`);
+      }
+
+      const hashedPassword = Users.hashPassword(req.body.password);
+
+      const newUser = new Users({
+        fullName: req.body.fullName,
+        // .create takes and object based on schema
+        username: req.body.username, // remember 'req.body' is request that user sends
+        password: hashedPassword, // Hashes password entered  when registering before storing in MongoDB
+        email: req.body.email,
+        breweries: [],
+      });
+      // Validate and save the beer
+      await newUser.validate();
+      const savedUser = await newUser.save();
+
+      res.status(201).json({ savedUser });
+    } catch (error) {
+      handleError(res, error);
+    }
+  }
+);
+
+/**
+ * POST: Creates new brewery; Company Name & Owner are required fields!
+ * Request body: Bearer token, JSON with user information
+ * @returns brewery object
+ */
+app.post(
+  "/:user/breweries",
+  [
+    // Validation logic
+    //minimum value of 5 characters are only allowed
+    check("companyName", "Company Name is required").not().isEmpty(),
+
+    // field can only contain letters and numbers
+    // check(
+    //   "companyName",
+    //   "Company Name contains non alphanumeric characters - not allowed."
+    // ).isAlphanumeric(),
+  ],
+  async (req, res) => {
+    // check the validation object for errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await Users.findById(req.params.user).populate();
+      if (!user) {
+        return res.status(400).send("User not found");
+      }
+
+      // Check if category already exists
+      const existingBrewery = user.breweries.find(
+        (brewery) => brewery.companyName === req.body.companyName
+      );
+      if (existingBrewery) {
+        return res.status(400).send("Brewery already exists");
+      }
+
+      const brewery = new Breweries({
+        companyName: req.body.companyName,
+        owner: req.params.user,
+        admin: [req.params.user],
+        staff: [],
+        beers: [],
+        categories: [],
+      });
+
+      // Validate and save the beer
+      await brewery.validate();
+      const savedBrewery = await brewery.save();
+
+      if (savedBrewery) {
+        user.breweries.push(savedBrewery._id);
+        await user.save();
+        res.status(201).json({ savedBrewery });
+      } else {
+        throw new Error("Brewery save operation failed");
+      }
+    } catch (error) {
+      handleError(res, error);
+    }
   }
 );
 
@@ -142,7 +209,7 @@ app.post(
  * @returns beer object
  */
 app.post(
-  "/:user/:brewery/beers",
+  "/:brewery/beers",
   [
     // passport.authenticate("jwt", { session: false }),
     // Validation logic
@@ -154,25 +221,15 @@ app.post(
   ],
   async (req, res) => {
     // check the validation object for errors
-    let errors = validationResult(req);
-
+    const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() });
     }
 
     try {
-      const user = await Users.findById(req.params.user);
-      if (!user) {
-        return res.status(400).send("User not found");
-      }
-
-      const brewery = await Breweries.findOne({
-        _id: req.params.brewery,
-        $or: [{ staff: { $in: [user._id] } }, { owner: user._id }],
-      });
-
+      const brewery = await Breweries.findById(req.params.brewery);
       if (!brewery) {
-        return res.status(400).send("User is not an admin or owner");
+        return res.status(400).send("Brewery not found");
       }
 
       const beer = new Beers({
@@ -188,10 +245,7 @@ app.post(
         notes: req.body.notes,
       });
 
-      /* using save() instead of create() allows us 
-      to check if the beer document is valid 
-      before saving to database 
-      */
+      // Validate and save the beer
       await beer.validate();
       const savedBeer = await beer.save();
 
@@ -203,7 +257,66 @@ app.post(
         throw new Error("Beer save operation failed");
       }
     } catch (error) {
-      handleError(error);
+      handleError(res, error);
+    }
+  }
+);
+
+/**
+ * POST: Creates new brewery category; Name is required fields!
+ * Request body: Bearer token, JSON with user information
+ * @returns category object
+ */
+app.post(
+  "/:brewery/categories",
+  [
+    // passport.authenticate("jwt", { session: false }),
+    // Validation logic
+    //minimum value of 1 characters are only allowed
+    check("name", "Category name is required").isLength({ min: 1 }),
+  ],
+  async (req, res) => {
+    // check the validation object for errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+    try {
+      // .populates makes sure we have full objects, not just id
+      const brewery = await Breweries.findById(req.params.brewery).populate(
+        "categories"
+      );
+      if (!brewery) {
+        return res.status(400).send("Brewery not found");
+      }
+      console.log(brewery);
+      // Check if category already exists
+      const existingCategory = brewery.categories.find(
+        (category) => category.name === req.body.name
+      );
+      if (existingCategory) {
+        return res.status(400).send("Category already exists in the brewery");
+      }
+
+      const category = new Categories({
+        name: req.body.name,
+      });
+
+      // Validate and save the beer
+      await category.validate();
+
+      const savedCategory = await category.save();
+
+      if (savedCategory) {
+        brewery.categories.push(savedCategory._id);
+        await brewery.save();
+        res.status(201).json({ savedCategory });
+      } else {
+        throw new Error("Category save operation failed");
+      }
+    } catch (error) {
+      handleError(res, error);
     }
   }
 );
@@ -229,6 +342,26 @@ app.get(
 );
 
 /**
+ * GET: Returns data on a single user (user object) by user username
+ * Request body: Bearer token
+ * @param username
+ * @returns user object
+ * @requires passport
+ */
+app.get(
+  "/users/:username",
+  // passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    // condition to find specific user based on username
+    Users.findOne({ username: req.params.username })
+      .then((user) => {
+        res.json(user);
+      })
+      .catch(handleError);
+  }
+);
+
+/**
  * GET: Returns a list of ALL breweries
  * Request body: Bearer token
  * @returns array of brewery objects
@@ -241,6 +374,246 @@ app.get(
     Breweries.find() // .find() grabs data on all documents in collection
       .then((brewery) => {
         res.status(201).json(brewery);
+      })
+      .catch(handleError);
+  }
+);
+
+/**
+ * GET: Returns data on a single brewery (brewery object) by brewery id
+ * Request body: Bearer token
+ * @param brewery
+ * @returns brewery object
+ * @requires passport
+ */
+app.get(
+  "/breweries/:breweryId",
+  // passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    // condition to find specific brewery based on _id
+    Breweries.findOne({ _id: req.params.breweryId })
+      .then((brewery) => {
+        res.json(brewery);
+      })
+      .catch(handleError);
+  }
+);
+
+/**
+ * GET: Returns a list of ALL beers
+ * Request body: Bearer token
+ * @returns array of beer objects
+ * @requires passport
+ */
+app.get(
+  "/beers",
+  // passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Beers.find() // .find() grabs data on all documents in collection
+      .then((beers) => {
+        res.status(201).json(beers);
+      })
+      .catch(handleError);
+  }
+);
+
+/**
+ * GET: Returns a list of ALL categories
+ * Request body: Bearer token
+ * @returns array of categories objects
+ * @requires passport
+ */
+app.get(
+  "/categories",
+  // passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Categories.find() // .find() grabs data on all documents in collection
+      .then((categories) => {
+        res.status(201).json(categories);
+      })
+      .catch(handleError);
+  }
+);
+
+//  PUT/ UPDATE REQUEST ********************
+
+/**
+ * PUT: Update user info
+ * Request body: Bearer token, updated user info
+ * @param userID
+ * @returns user object with updates
+ * @requires passport
+ */
+app.put(
+  "/users/:userId",
+  [
+    // Validation logic
+    // passport.authenticate("jwt", { session: false }),
+    // Minimum value of 5 characters is required
+    check("username", "Username is required").isLength({ min: 5 }),
+
+    // Field can only contain letters and numbers
+    check(
+      "username",
+      "Username contains non-alphanumeric characters - not allowed."
+    ).isAlphanumeric(),
+
+    // Password is required
+    check("password", "Password is required").not().isEmpty(),
+
+    // Field must be formatted as an email address
+    check("email", "Email does not appear to be valid").isEmail(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+    try {
+      const userId = req.params.userId;
+      const updateFields = {
+        fullName: req.body.fullName,
+        username: req.body.username,
+        password: req.body.password,
+        email: req.body.email,
+        breweries: req.body.breweries,
+      };
+
+      const existingUser = await Users.findByIdAndUpdate(userId, updateFields, {
+        new: true,
+      });
+
+      if (!existingUser) {
+        return res.status(400).send("User not found");
+      }
+
+      res.status(200).json({ existingUser });
+    } catch (error) {
+      handleError(res, error);
+    }
+  }
+);
+
+/**
+ * PUT: Update beer info
+ * Request body: Bearer token, updated beer info
+ * @param beerId
+ * @returns beer object with updates
+ * @requires passport
+ */
+app.put(
+  "/:brewery/beers/:beerId",
+  [
+    // Validation logic
+    // passport.authenticate("jwt", { session: false }),
+    //minimum value of 5 characters are only allowed
+    check("name", "Name is required").not().isEmpty(),
+
+    // Chain of methods like .not().isEmpty() which means "opposite of isEmpty" or "is not empty"
+    check("style", "Style is required").not().isEmpty(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+    try {
+      const beerId = req.params.beerId;
+
+      const updateFields = {
+        name: req.body.name,
+        style: req.body.style,
+        abv: req.body.abv,
+        ibu: req.body.ibu,
+        category: req.body.category,
+        malt: req.body.malt,
+        hops: req.body.hops,
+        flavorNotes: req.body.flavorNotes,
+        aroma: req.body.aroma,
+        nameSake: req.body.nameSake,
+        notes: req.body.notes,
+      };
+
+      // updates by id and only fields that have changed
+      const existingBeer = await Beers.findByIdAndUpdate(beerId, updateFields, {
+        new: true,
+      });
+
+      if (!existingBeer) {
+        return res.status(400).send("Beer not found");
+      }
+
+      res.status(200).json({ existingBeer });
+    } catch (error) {
+      handleError(res, error);
+    }
+  }
+);
+
+app.put(
+  "/breweries/:breweryId",
+  [
+    // Validation logic
+    // passport.authenticate("jwt", { session: false }),
+    // Minimum value of 1 character is required
+    check("companyName", "Company Name is required").isLength({ min: 1 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+    try {
+      const breweryId = req.params.breweryId;
+      const updateFields = {
+        companyName: req.body.companyName,
+        owner: req.body.owner,
+        admin: req.body.admin,
+        staff: req.body.staff,
+        beers: req.body.beers,
+        categories: req.body.categories,
+      };
+
+      const existingBrewery = await Breweries.findByIdAndUpdate(
+        breweryId,
+        updateFields,
+        { new: true }
+      );
+
+      if (!existingBrewery) {
+        return res.status(400).send("Brewery not found");
+      }
+
+      res.status(200).json({ existingBrewery });
+    } catch (error) {
+      handleError(res, error);
+    }
+  }
+);
+
+//  DELETE REQUEST *****************
+/**
+ * DELETE: Deletes brewery
+ * Request body: Bearer token
+ * @param brewery
+ * @returns success message
+ * @requires passport
+ */
+app.delete(
+  "/breweries/:brewery",
+  // passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Breweries.findOneAndRemove({ _id: req.params.brewery })
+      .populate("companyName")
+      .then((brewery) => {
+        if (!brewery) {
+          res.status(400).send(`${brewery.companyName} was not found.`);
+        } else {
+          res.status(200).send(`${brewery.companyName} was deleted.`);
+        }
       })
       .catch(handleError);
   }

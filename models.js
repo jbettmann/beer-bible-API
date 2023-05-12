45622b64e8084549155ad0f1affa136cb6fc2a03
jbettmann <jordan.bettmann@gmail.com>
2023-05-11 21:28:25 -0600

    added default to breweries property in user

--- models.js
+++ models.js
@@ -7,7 +7,9 @@ const userSchema = mongoose.Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
   email: { type: String, required: true },
-  breweries: [{ type: mongoose.Schema.Types.ObjectId, ref: "Brewery" }],
+  breweries: [
+    { type: mongoose.Schema.Types.ObjectId, ref: "Brewery", default: [] },
+  ],
 });
 
 // brewery schema

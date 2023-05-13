57798669d5316d3851852bf9d52d59eb663d15f5
jbettmann <jordan.bettmann@gmail.com>
2023-05-12 21:30:11 -0600

    Update passport.js

--- passport.js
+++ passport.js
@@ -1,54 +1,64 @@
-
-
-const passport = require('passport'),
-  LocalStrategy = require('passport-local').Strategy,
-  Models = require('./models.js'),
-  passportJWT = require('passport-jwt');
+const passport = require("passport"),
+  LocalStrategy = require("passport-local").Strategy,
+  Models = require("./models.js"),
+  passportJWT = require("passport-jwt");
 
 let Users = Models.User,
-  JWTStrategy = passportJWT.Strategy, 
+  JWTStrategy = passportJWT.Strategy,
   ExtractJWT = passportJWT.ExtractJwt;
 
-// HTTP Authentication. Takes username and password from request body and usses Mongoose to check database
-passport.use(new LocalStrategy({
-  usernameField: 'Username',
-  passwordField: 'Password' // password doesnt not get check here. 
-  // if match, callback is executed. This is login endpoint
-}, (username, password, callback) => {
-  console.log(username + '  ' + password);
-  Users.findOne({ Username: username }, (error, user) => {
-    // if error occures
-    if (error) {  
-      console.log(error);
-      return callback(error);
+// HTTP Authentication. Takes username and password from request body and uses Mongoose to check database
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "username",
+      passwordField: "password", // password doesnt not get check here.
+      // if match, callback is executed. This is login endpoint
+    },
+    (username, password, callback) => {
+      console.log(username + "  " + password);
+      Users.findOne({ username: username }, (error, user) => {
+        // if error occures
+        if (error) {
+          console.log(error);
+          return callback(error);
+        }
+        // in no user is found, message passed to callback.
+        if (!user) {
+          console.log("incorrect username");
+          return callback(null, false, {
+            message: "Incorrect username or password.",
+          });
+        }
+        // Hashes password enterd when logging in before comparing to password in database
+        if (!user.validatePassword(password)) {
+          console.log("incorrect password");
+          return callback(null, false, { message: "Incorrect password." });
+        }
+        console.log("finished");
+        return callback(null, user);
+      });
     }
-     // in no user is found, message passed to callback.
-    if (!user) {
-      console.log('incorrect username');
-      return callback(null, false, {message: 'Incorrect username or password.'});
-    }
-    // Hashes password enterd when logging in before comparing to password in database
-    if (!user.validatePassword(password)) {
-        console.log('incorrect password');
-        return callback(null, false, {message: 'Incorrect password.'});
-    }
-    console.log('finished');
-    return callback(null, user);
-  });
-}));
+  )
+);
 
 // Authenticates users on JWT summited alongside request
-passport.use(new JWTStrategy({
-    // Extracts JWT from header of HTTP request.
-  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-    // Verifies singature of JWT. Client is who it says it is.
-  secretOrKey: 'your_jwt_secret'
-}, (jwtPayload, callback) => {
-  return Users.findById(jwtPayload._id)
-    .then((user) => {
-      return callback(null, user);
-    })
-    .catch((error) => {
-      return callback(error)
-    });
-}));
+passport.use(
+  new JWTStrategy(
+    {
+      // Extracts JWT from header of HTTP request.
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+      // Verifies singature of JWT. Client is who it says it is.
+      secretOrKey: "your_jwt_secret",
+    },
+    (jwtPayload, callback) => {
+      return Users.findById(jwtPayload._id)
+        .then((user) => {
+          return callback(null, user);
+        })
+        .catch((error) => {
+          return callback(error);
+        });
+    }
+  )
+);

5dfcfcaa02672d85618b581b4fefeb0deb4fe673
jbettmann <jordan.bettmann@gmail.com>
2023-05-19 10:01:50 -0600

    added vercel config code

--- index.js
+++ index.js
@@ -1119,3 +1119,6 @@ const port = process.env.PORT || 8080;
 app.listen(port, "0.0.0.0", () => {
   console.log(`Listening on Port ${port}`);
 });
+
+// Export the Express API
+module.exports = app;

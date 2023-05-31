15fe7a80d875514fd57102414336d2dbdec801ce
jbettmann <jordan.bettmann@gmail.com>
2023-05-31 15:12:34 -0600

    Update index.js

--- index.js
+++ index.js
@@ -470,17 +470,13 @@ app.post(
  * @returns array of user objects
  * @requires passport
  */
-app.get(
-  "/users",
-  // verifyJWT,
-  (req, res) => {
-    Users.find() // .find() grabs data on all documents in collection
-      .then((users) => {
-        res.status(201).json(users);
-      })
-      .catch(handleError);
-  }
-);
+app.get("/users", verifyJWT, (req, res) => {
+  Users.find() // .find() grabs data on all documents in collection
+    .then((users) => {
+      res.status(201).json(users);
+    })
+    .catch(handleError);
+});
 
 /**
  * GET: Returns data on a single user (user object) by user username

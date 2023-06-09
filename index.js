0414cbd5d4d4a9452a348b160cd66c6c5f8a879f
jbettmann <jordan.bettmann@gmail.com>
2023-06-09 09:30:09 -0600

    Update index.js

--- index.js
+++ index.js
@@ -279,9 +279,9 @@ app.post(
 
       const brewery = new Breweries({
         companyName: req.body.companyName,
-        owner: req.user._id,
-        admin: [req.user._id, req.params.admin],
-        staff: [req.user._id, req.params.staff],
+        owner: req.params.user,
+        admin: [req.params.user, req.params.admin],
+        staff: [req.params.user, req.params.staff],
         beers: [],
         categories: [],
       });

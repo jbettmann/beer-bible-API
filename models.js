99977ba757bbf82c93079d8f89197ed2c41b92c5
jbettmann <jordan.bettmann@gmail.com>
2023-06-09 09:44:33 -0600

    Update models.js

--- models.js
+++ models.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose"),
 const userSchema = mongoose.Schema({
   fullName: { type: String, required: true },
   username: { type: String, required: true },
-  password: { type: String, required: true },
+  password: { type: String },
   email: { type: String, required: true },
   breweries: [
     { type: mongoose.Schema.Types.ObjectId, ref: "Brewery", default: [] },

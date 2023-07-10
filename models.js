19d75035f11420bf15dd502cac40b8fba6bec2bb
jbettmann <jordan.bettmann@gmail.com>
2023-07-10 09:39:51 -0600

    removed username & password

--- models.js
+++ models.js
@@ -4,8 +4,6 @@ const mongoose = require("mongoose"),
 // user schema
 const userSchema = mongoose.Schema({
   fullName: { type: String, required: true },
-  username: { type: String, required: true },
-  password: { type: String },
   email: { type: String, required: true },
   breweries: [
     { type: mongoose.Schema.Types.ObjectId, ref: "Brewery", default: [] },
